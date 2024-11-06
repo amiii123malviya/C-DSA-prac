aca0af5c8c8e71154b34933788c9ed6e4bf4731f
amisha <amishamalviya2607@gmail.com>
2024-11-06 20:48:55 +0530

    First_commit_to_thi_repo

---
+++ amii.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int c[5];
+   for (int a=1; a<6;a++)
+   {
+    cin>>c[a];
+   }
+    
+   for(int b=1;b<6;b++)
+   {
+cout<<c[b];
+   }
+
+
+} 
+
+
