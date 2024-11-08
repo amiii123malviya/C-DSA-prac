b91adf4c42e46c7bf8cedc658ef688e749cdc46e
amisha <amishamalviya2607@gmail.com>
2024-11-08 22:36:44 +0530

    Adding_insertion_sort_revise_file_will_update

---
+++ insertion.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[5],temp;
+    for(int i=0;i<5;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"output"<<"\n";
+    for(int i=0;i<5;i++)
+    {
+        cout<<arr[i]<<"\n";
+    }
+    // to be continued and do the steps of swapping tommorow
+}
